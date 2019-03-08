67011446bff4b4dccb2c38376dc8061297560611
pankaj99com <pankaj99.com@gmail.com>
2019-03-08 14:38:01 +0530

    new

--- JavaDay1/src/demo.java
+++ JavaDay1/src/demo.java
@@ -14,6 +14,9 @@ public class demo {
 	public static void main(String[] args) {//This program will print duplicates
 		int[]a={2,7,6,5,8,3,7,9,9,7,4,3};
 		
+		
+		
+		
 		for(int i=0;i<a.length;i++){
 			for(int j=i+1;j<a.length;j++){
 				
