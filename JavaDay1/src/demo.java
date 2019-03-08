a51d381f5194ab9af9a3c65e5c39ab21d67af8d0
pankaj99com <pankaj99.com@gmail.com>
2019-03-08 13:39:25 +0530

    New comment added

--- JavaDay1/src/demo.java
+++ JavaDay1/src/demo.java
@@ -11,13 +11,14 @@ public class demo {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {//This program will print duplicates
 		int[]a={2,7,6,5,8,3,7,9,9,7,4,3};
 		
 		for(int i=0;i<a.length;i++){
 			for(int j=i+1;j<a.length;j++){
 				
 				if((a[i]==a[j]) && (i!=j)){
+					
 					System.out.print(a[j]+" ");
 					
 				}
